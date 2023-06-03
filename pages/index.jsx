f723093770c69c423c51a058062588e095bbff8b
muhammadrifatba <muhammadrifatba@gmail.com>
2023-06-03 21:18:25 +0700

    first commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
 
   useEffect(() => {
     const video = document.createElement("video");
-    const canPlayAV1 = video.canPlayType("video/webm; codecs=\"av01.0.05M.08\"");
+    const canPlayAV1 = video.canPlayType('video/webm; codecs="av01.0.05M.08"');
     if (canPlayAV1 === "probably") {
       setSupportAV1(true);
     } else {
@@ -33,15 +33,18 @@ export default function Home() {
             autoPlay
             muted
             loop
-            className="absolute h-full w-full bg-clip-content object-cover opacity-40"
+            className="#0F222F absolute h-full w-full bg-clip-content object-cover opacity-70"
           >
             {supportAV1 ? (
               <source
-                src="/homepage/video_v3.av1.webm"
+                src="/homepage/Abstract Dark Blue moving Circles and Dots Animated Background Video.webm"
                 type="video/webm; codecs=av01"
               />
             ) : (
-              <source src="/homepage/video_v3.mp4" type="video/mp4" />
+              <source
+                src="/homepage/Abstract Dark Blue moving Circles and Dots Animated Background Video.mp4"
+                type="video/mp4"
+              />
             )}
           </video>
           <RevealWrapper
@@ -54,21 +57,21 @@ export default function Home() {
             <div className="relative z-10 flex h-screen items-center justify-center bg-cover">
               <div className="flex h-max w-max flex-col flex-wrap items-center justify-center md:flex-row">
                 <Image
-                  src="/logo-anjangsana.webp"
-                  alt="logo anjangsana"
-                  className="mt-auto h-[30vw] w-auto md:-ml-8 md:h-[35vh] xl:-ml-20 xl:h-[55vh]"
+                  src="/homepage/ticket.webp"
+                  alt="logo event"
+                  className="mt-auto h-[20vw] w-auto md:-ml-8 md:h-[35vh] xl:-ml-20 xl:h-[45vh]"
                   width={1000}
                   height={1000}
                 />
                 <div className="w-max text-center md:text-left">
-                  <h2 className="text-3xl font-bold text-white drop-shadow md:text-5xl xl:text-8xl ">
-                    GRAND CONCERT
+                  <h2 className="text-3xl font-bold text-white drop-shadow md:text-5xl xl:text-7xl ">
+                    Jogja Mengsedih
                   </h2>
                   <p className="mb-2 font-normal text-white drop-shadow md:mb-6 md:text-4xl xl:mb-16 xl:text-6xl">
                     Vol. 10
                   </p>
                   <p className="font-normal text-white drop-shadow md:text-2xl">
-                    Sabtu, <b>27</b> Mei 2023 @ <b>17.30 WIB</b>
+                    Sabtu, <b>10</b> Juni 2023 @ <b>19.00 WIB</b>
                     <br />
                     Auditorium Driyarkara Sanata Dharma
                   </p>
@@ -86,60 +89,8 @@ export default function Home() {
           </RevealWrapper>
         </div>
 
-        {/* Home Content 1 */}
-        <div
-          id="about"
-          className="container mx-auto grid w-full grid-cols-1 items-center justify-between px-10 py-20 lg:grid-cols-2"
-        >
-          {/* GMCO Image */}
-          <RevealWrapper origin="left" duration={1000} distance="100px">
-            <div className="w-full">
-              <div className="lg:w-3/4">
-                <Image
-                  src="/logo_gmco.webp"
-                  alt="logo gmco.webp"
-                  className="mx-auto h-auto w-full"
-                  width={500}
-                  height={500}
-                />
-              </div>
-            </div>
-          </RevealWrapper>
-
-          {/* Text */}
-          <RevealWrapper duration={1000} distance="0">
-            <div className="w-full">
-              <div className="py-12 text-center lg:text-left">
-                <div className="text-md font-bold uppercase tracking-wide text-gmco-orange-secondarylight md:text-xl">
-                  About
-                </div>
-                <h1 className="mb-4 mt-2 text-2xl font-semibold leading-tight text-gray-900 md:text-4xl">
-                  Gadjah Mada Chamber Orchestra
-                </h1>
-                <p className="text-md mb-4 leading-normal text-gray-900 md:text-lg">
-                  Gadjah Mada Chamber Orchestra (GMCO) kembali hadir menyambut
-                  para penikmat musik dalam &quot; Grand Concert Vol. 10:
-                  Anjangsana Simfoni &quot;. Anjangsana Simfoni berarti melepas
-                  rindu kidung masa kecil melalui alunan simfoni orkestra oleh
-                  Gadjah Mada Chamber Orchestra. Penampilan ini akan mengundang
-                  kembali anggota-anggota yang telah maupun sedang berkarya di
-                  Gadjah Mada Chamber Orchestra.
-                </p>
-                <a
-                  className="text-gmco-white"
-                  href="https://gmco.ukm.ugm.ac.id/"
-                >
-                  <button className="delay-15 rounded-md bg-gmco-blue-main px-4 py-2 transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-gmco-orange-secondarydark md:px-6 md:py-3">
-                    More About GMCO
-                  </button>
-                </a>
-              </div>
-            </div>
-          </RevealWrapper>
-        </div>
-
         {/* Home Content2 - Addie MS */}
-        <div className="bg-[#191919] py-16">
+        <div className="bg-gmco-white py-16">
           <div
             id="GS"
             className="container mx-auto grid grid-cols-1 items-center justify-between px-10 lg:grid-cols-2"
@@ -148,14 +99,14 @@ export default function Home() {
             <RevealWrapper
               duration={1000}
               distance="0"
-              className="order-last flex w-full justify-end text-gmco-white lg:order-first"
+              className="text-gmco-gray order-last flex w-full justify-end lg:order-first"
             >
               <div className="py-12 text-center md:text-left lg:w-3/4">
                 <div className="text-md font-bold tracking-wide md:text-2xl">
                   Featuring,
                 </div>
                 <h1 className="mb-4 mt-2 text-2xl font-extrabold leading-tight md:text-7xl">
-                  Addie MS{" "}
+                  Tulus{" "}
                   <span className="text-gmco-orange-secondarydark">.</span>
                 </h1>
                 <p className="mb-4 text-justify text-base font-light leading-normal opacity-80 md:text-lg">
@@ -179,7 +130,7 @@ export default function Home() {
                   </b>
                 </p>
                 <a
-                  className="delay-15 border-b-2 py-3 text-gmco-white transition duration-300 ease-in-out hover:border-gmco-orange-secondarylight"
+                  className="delay-15 text-gmco-gray border-b-2 py-3 transition duration-300 ease-in-out hover:border-gmco-orange-secondarylight"
                   href="https://id.wikipedia.org/wiki/Addie_MS"
                 >
                   More About Addie MS
@@ -195,7 +146,7 @@ export default function Home() {
             >
               <div className="flex h-[50vh] items-center md:h-[70vh] md:p-8">
                 <Image
-                  src="/homepage/addiems_clearnew.png"
+                  src="/homepage/tulus_nobg.png"
                   alt="gambar addie ms"
                   className="mx-auto max-h-full w-full overflow-visible object-cover"
                   width={500}
@@ -207,8 +158,8 @@ export default function Home() {
         </div>
 
         {/* Home Content 3 - Brian*/}
-        <div className="bg-gmco-white py-16">
-          <div className="container mx-auto grid grid-cols-1 items-center justify-between px-10 lg:grid-cols-2">
+        <div className="bg-[#191919] py-16">
+          <div className="container mx-auto grid grid-cols-1 items-center justify-between px-10 text-gmco-white lg:grid-cols-2">
             {/* GMCO Image */}
             <RevealWrapper
               origin="left"
@@ -231,7 +182,7 @@ export default function Home() {
             <RevealWrapper
               duration={1000}
               distance="0"
-              className="flex w-full justify-start text-gmco-grey"
+              className="flex w-full justify-start text-gmco-white"
             >
               <div className="py-12 text-center md:text-left lg:w-3/4">
                 <div className="text-md font-bold tracking-wide md:text-2xl">
@@ -257,7 +208,7 @@ export default function Home() {
                   <b className="font-bold">Sudah</b>.
                 </p>
                 <a
-                  className="delay-15 border-b-2 border-gmco-orange-secondarylight py-3 text-gmco-grey transition duration-300 ease-in-out hover:border-gmco-grey"
+                  className="delay-15 border-b-2 border-gmco-orange-secondarylight py-3 text-gmco-white transition duration-300 ease-in-out hover:border-gmco-grey"
                   href="https://open.spotify.com/artist/17Yb156MR5C8M2Ktr22mww"
                 >
                   More About Brian Prasetyoadi
